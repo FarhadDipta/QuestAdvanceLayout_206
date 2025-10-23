79c258469603cd0b1e05d153dd10a2a98310d97f
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2025-10-23 09:24:53 +0700

    Menambahkan import dasar Compose

--- app/src/main/java/com/example/praktikum2/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/praktikum2/ui/theme/UIPertama.kt
@@ -1,1 +1,6 @@
 package com.example.praktikum2
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
