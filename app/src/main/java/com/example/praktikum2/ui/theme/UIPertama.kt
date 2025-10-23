77bde60aec9940b22268337387974fe626312a8d
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2025-10-23 09:37:35 +0700

    Menambahkan Text footer di bagian bawah layar

--- app/src/main/java/com/example/praktikum2/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/praktikum2/ui/theme/UIPertama.kt
@@ -70,6 +70,12 @@ fun AktivitasPertama(modifier: Modifier) {
         Box(
             modifier = Modifier.fillMaxSize()
         ) {
+            Text(
+                text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
         }
     }
 }
